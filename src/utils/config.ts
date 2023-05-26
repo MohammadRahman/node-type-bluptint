a287573746df9389faf7116e1b68d94f649cd369
MohammadRahman <45675266+MohammadRahman@users.noreply.github.com>
2023-05-26 14:36:28 +0300

    config file updated

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,18 +5,7 @@ const schema = Type.Object({
     PORT: Type.Number({
         default: 3001
     }),
-    ADMIN_DATABASE_URL: Type.String({
-        default: process.env.ADMIN_DATABASE_URL
-    }),
-    DATABASE_USER: Type.String({
-        default: process.env.DATABASE_USER
-    }),
-    DATABASE_PASSWORD: Type.String({
-        default: process.env.DATABASE_PASSWORD
-    }),
-    CUSTOMER_DATABASE_URL: Type.String({
-        default: process.env.CUSTOMER_DATABASE_URL
-    }),
+    DB_URL: Type.String()
 })
 
 type Env = Static<typeof schema>
--- src/utils/dbConfig.ts
+++ src/utils/dbConfig.ts
@@ -3,13 +3,9 @@ import { config } from "./config";
 import { log } from "./logger";
 
 export async function connectDatabase() {
-    const uri = config.ADMIN_DATABASE_URL
+    const uri = config.DB_URL
     try {
-        const con = await mongoose.createConnection(uri, {
-            user: config.DATABASE_USER,
-            pass: config.DATABASE_PASSWORD,
-            dbName: config.CUSTOMER_DATABASE_URL
-        })
+        const con = await mongoose.createConnection(uri)
         log.info('DATABSE CONNECTED')
     } catch (error) {
         log.error(error)
